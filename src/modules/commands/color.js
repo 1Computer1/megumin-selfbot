fbfb70bb9dcab73bd2e2c273a1d6fc4ad4ef1865
1Computer1 <mineox100@gmail.com>
2017-02-05 20:37:58 -0500

    Added color command.

---
+++ src/modules/commands/color.js
@@ -0,0 +1,23 @@
+const { Command } = require('discord-akairo');
+const { RichEmbed } = require('discord.js');
+
+function exec(message, args){
+    const embed = new RichEmbed().setColor(args.content);
+
+    let hex = isNaN(embed.color) ? '0' : embed.color.toString(16);
+    hex = '#' + '0'.repeat(6 - hex.length) + hex;
+    embed.setDescription(hex.toUpperCase());
+
+    return message.edit('', { embed });
+}
+
+module.exports = new Command('color', exec, {
+    aliases: ['color', 'colour'],
+    args: [
+        {
+            id: 'content',
+            match: 'content',
+            defaultValue: () => (1 << 24) * Math.random() | 0
+        }
+    ]
+});
