94c1288702a50a0e32cd94a67ee27f06cdc6c657
1Computer1 <mineox100@gmail.com>
2017-02-16 00:55:26 -0500

    Update for Akairo 5.1.0.

---
+++ src/modules/listeners/error.js
@@ -0,0 +1,12 @@
+const { Listener } = require('discord-akairo');
+
+function exec(err){
+    this.client.logger.log(3, 'An error occured. Contact someone who might know what it means.');
+    console.error(err);
+}
+
+module.exports = new Listener('error', exec, {
+    emitter: 'commandHandler',
+    eventName: 'error',
+    type: 'on'
+});
