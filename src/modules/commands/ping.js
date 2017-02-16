e0dece8771616cd7b03cfc017fcef29e0e48e60e
1Computer1 <mineox100@gmail.com>
2017-02-16 00:53:10 -0500

    Prettier ping.

--- src/modules/commands/ping.js
+++ src/modules/commands/ping.js
@@ -1,9 +1,9 @@
 const { Command } = require('discord-akairo');
 
 function exec(message){
-    return message.edit('Pong!').then(m => {
+    return message.edit('🏓\u2000Pong!').then(m => {
         const diff = m.editedAt - m.createdAt;
-        return m.edit(`Pong! (${diff} ms)`);
+        return m.edit(`🏓\u2000Pong! (${diff} ms)`);
     });
 }
 
