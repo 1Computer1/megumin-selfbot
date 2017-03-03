7fde9f4e19fe7f1103ddd99fcbee1f9a683f010e
1Computer1 <mineox100@gmail.com>
2017-03-03 18:37:09 -0500

    Log close code when disconnect

--- src/modules/listeners/disconnect.js
+++ src/modules/listeners/disconnect.js
@@ -1,7 +1,7 @@
 const { Listener } = require('discord-akairo');
 
-function exec(){
-    return this.client.logger.log(1, 'Client disconnected.');
+function exec(close){
+    return this.client.logger.log(1, `Client disconnected with code ${close.code}.`);
 }
 
 module.exports = new Listener('disconnect', exec, {
