be75a3cb7742a26cee9c39521c55bc9846204feb
1Computer1 <mineox100@gmail.com>
2017-03-04 19:15:13 -0500

    Add print() from async to eval

--- src/modules/commands/util/eval.js
+++ src/modules/commands/util/eval.js
@@ -10,25 +10,51 @@ function exec(message, args){
         return message.delete();
     }
 
-    const result = new Promise(resolve => resolve(eval(args.code)));
-    const cb = '```';
+    const evaled = {};
+    const logs = [];
 
     const tokenRegex = new RegExp(this.client.token.replace(/\./g, '\\.').split('').join('.?'), 'g');
 
+    const print = (...a) => {
+        const cleaned = a.map(o => {
+            if (typeof o !== 'string') o = util.inspect(o);
+            return o.replace(tokenRegex, '[TOKEN]');
+        });
+
+        if (!evaled.output) return logs.push(...cleaned);
+
+        evaled.output += evaled.output.endsWith('\n') ? cleaned.join(' ') : `\n${cleaned.join(' ')}`;
+        const title = evaled.errored ? '☠\u2000**Error**' : '📤\u2000**Output**';
+
+        if (evaled.output.length + args.code.length > 1900) evaled.output = 'Output too long.';
+        return message.edit(`📥\u2000**Input**${cb}js\n${args.code}\n${cb}\n${title}${cb}js\n${evaled.output}\n${cb}`);
+    };
+
+    const result = new Promise(resolve => resolve(eval(args.code)));
+    const cb = '```';
+
     return result.then(output => {
         if (typeof output !== 'string') output = util.inspect(output);
+        output = `${logs.join('\n')}\n${logs.length && output === 'undefined' ? '' : output}`;
         output = output.replace(tokenRegex, '[TOKEN]');
 
         if (output.length + args.code.length > 1900) output = 'Output too long.';
-        return message.edit(`📥\u2000**Input**${cb}js\n${args.code}\n${cb}\n📤\u2000**Output**${cb}js\n${output}\n${cb}`);
+        return message.edit(`📥\u2000**Input**${cb}js\n${args.code}\n${cb}\n📤\u2000**Output**${cb}js\n${output}\n${cb}`).then(() => {
+            evaled.errored = false;
+            evaled.output = output;
+        });
     }).catch(err => {
         this.client.logger.log(2, 'Evaluation errored.');
         console.error(err);
 
         err = err.toString();
+        err = `${logs.join('\n')}\n${logs.length && err === 'undefined' ? '' : err}`;
         err = err.replace(tokenRegex, '[TOKEN]');
 
-        return message.edit(`📥\u2000**Input**${cb}js\n${args.code}\n${cb}\n☠\u2000**Error**${cb}js\n${err}\n${cb}`);
+        return message.edit(`📥\u2000**Input**${cb}js\n${args.code}\n${cb}\n☠\u2000**Error**${cb}js\n${err}\n${cb}`).then(() => {
+            evaled.errored = true;
+            evaled.output = err;
+        });
     });
 }
 
