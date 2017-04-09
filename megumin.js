85fb2481ef16a3fe6883b9bbd36fd05c472b23c9
1Computer1 <mineox100@gmail.com>
2017-04-09 19:57:07 -0400

    Cleanup

--- megumin.js
+++ megumin.js
@@ -5,17 +5,16 @@ const config = require('./src/data/config.json');
 const client = new AkairoClient({
     prefix: config.prefix,
     allowMention: false,
+    handleEdits: true,
+    commandUtil: false,
     selfbot: true,
-    ownerID: config.ownerID,
     commandDirectory: './src/modules/commands',
-    listenerDirectory: './src/modules/listeners',
-    handleEdits: true
+    listenerDirectory: './src/modules/listeners'
 }, {
     maxMessageCache: config.cache == null ? 50 : config.cache,
     disableEveryone: true,
     disabledEvents: [
         'TYPING_START',
-        'TYPING_END',
         'MESSAGE_DELETE',
         'GUILD_MEMBER_UPDATE',
         'PRESENCE_UPDATE',
@@ -31,24 +30,21 @@ Object.assign(client, {
     tags: require('./src/data/tags.json'),
     images: require('./src/data/images.json'),
     color: message => {
-        return config.color === 'random'
-        ? (1 << 24) * Math.random() | 0
-        : client.config.color === 'auto'
-        ? message.guild
-        ? client.util.displayColor(message.member)
-        : 0
-        : client.config.color || 0;
+        if (config.color === 'random') return (1 << 24) * Math.random() | 0;
+
+        if (config.color === 'auto') {
+            return message.guild ? client.util.displayColor(message.member) : 0;
+        }
+
+        return client.config.color || 0;
     }
 });
 
 client.login(config.token).then(() => {
     client.logger.log(1, 'Megumin ready! Explooooooosion!');
-}).catch(err => {
-    client.logger.log(3, 'Could not login. Is your token correct?');
-    console.error(err);
 });
 
 process.on('unhandledRejection', err => {
     client.logger.log(3, 'An error occured. Contact someone who might know what it means.');
-    console.error(err);
+    console.error(err); // eslint-disable-line no-console
 });
